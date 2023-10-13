6905f18d123ac93f9dcabbd1a59b9b879f7a7106
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-10-13 13:24:22 +0530

    expense scripts

---
+++ jenkins-ip/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3 " {
+    bucket = "terraform-malleswari"
+    key = "misc/jenkins-ip/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_instance" "instance" {
+  instance_id = "i-0890cd4106f346585"
+}
+
+resource "aws_route53_record" "jenkins" {
+  name    = "jenkins.malleswaridevops.online"
+  type    = "A"
+  zone_id = "Z088606431E8311EPHBFV"
+  records = [data.aws_instance.instance.public_ip]
+  ttl = 10
+}
